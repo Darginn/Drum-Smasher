fb123eeb1a4503197347b3acde270a4b97325ee4
Darginn <jb112899@gmail.com>
2020-01-02 10:53:59 -0600

    Github Integrated with Unity

---
+++ Assets/Scripts/Note/NoteRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteRenderer : MonoBehaviour
+{
+    public SpriteData spriteData;
+    [System.Serializable]
+    public class SpriteData
+    {
+        public NoteSpriteData[] notes;
+    }
+    [System.Serializable]
+    public class NoteSpriteData
+    {
+        public Sprite normal, finisher;
+    }
+}
