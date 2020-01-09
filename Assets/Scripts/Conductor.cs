551efbee9b2f62005162702256f0d57e4f9841bb
Blade12629 <blade1234@web.de>
2020-01-09 13:06:52 +0100

    Added song position text

--- Assets/Scripts/Conductor.cs
+++ Assets/Scripts/Conductor.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace DrumSmasher
 {
@@ -14,7 +16,29 @@ namespace DrumSmasher
         public AudioSource MusicSource;
         public AudioSource HitSource;
         public float Offset;
-
+        public bool ReachedEnd;
+        public Text MusicPositionText;
+        private AudioClip _musicClip
+        {
+            get
+            {
+                return MusicSource.clip;
+            }
+        }
+        private TimeSpan _musicPosition
+        {
+            get
+            {
+                return TimeSpan.FromSeconds(MusicSource.time);
+            }
+        }
+        private TimeSpan _musicLength
+        {
+            get
+            {
+                return TimeSpan.FromSeconds(_musicClip.length);
+            }
+        }
         // Start is called before the first frame update
         void Start()
         {
@@ -31,6 +55,13 @@ namespace DrumSmasher
             SongPosition = (float)(AudioSettings.dspTime - DspSongTime - Offset);
 
             SongPositionInBeats = SongPosition / SecPerBeat;
+            
+            if (MusicSource.time <= _musicClip.length)
+            {
+                TimeSpan pos = _musicPosition;
+                TimeSpan length = _musicLength;
+                MusicPositionText.text = $"{pos.Hours}:{pos.Seconds}:{pos.Milliseconds}/{length.Hours}:{length.Seconds}:{length.Milliseconds}";
+            }
         }
     }
 }
