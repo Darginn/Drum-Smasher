969a1b88aec416b1ff886183107cb7020e200c9d
Darginn <jb112899@gmail.com>
2020-02-11 01:22:34 -0600

    Added highest FPS counter and another map for performance testing lol

--- Assets/Scripts/Debug/FPSCounter.cs
+++ Assets/Scripts/Debug/FPSCounter.cs
@@ -7,14 +7,19 @@ namespace DrumSmasher.Debug
     public class FPSCounter : MonoBehaviour
     {
         public int AvgFrameRate;
-        public Text DisplayText;
+        public int MaxFrameRate;
+        public Text FPSText;
+        public Text MAXFPSText;
 
         public void Update()
         {
             float current = 0;
             current = (int)(1f / Time.unscaledDeltaTime);
             AvgFrameRate = (int)current;
-            DisplayText.text = AvgFrameRate.ToString() + " FPS";
+            FPSText.text = AvgFrameRate.ToString() + " FPS";
+            if (MaxFrameRate < AvgFrameRate)
+                MaxFrameRate = AvgFrameRate;
+            MAXFPSText.text = "Max: " + MaxFrameRate.ToString() + " FPS";
         }
     }
 }
