73c8a9ebbd4792e8be2391ca84e35178c80c708a
Blade12629 <blade1234@web.de>
2020-02-21 11:54:54 +0100

    .

---
+++ Assets/Scripts/Charts/SongScanning.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using DrumSmasher.Charts;
+using System.Linq;
+using System;
+
+namespace DrumSmasher
+{
+    public class SongScanning : MonoBehaviour
+    {
+        public List<SongInfo> Songs;
+        private DirectoryInfo _songFolder;
+
+        public void Awake()
+        {
+            _songFolder = new DirectoryInfo(Application.dataPath + "/../Charts/");
+        }
+
+        public void RefreshSongs()
+        {
+            if (!_songFolder.Exists)
+            {
+                Logger.Log("Directory not found! Creating new one", LogLevel.WARNING);
+                _songFolder.Create();
+                Logger.Log($"Directory {_songFolder.FullName} created successfully");
+            }
+
+            Logger.Log($"Found {_songFolder.FullName}");
+            Songs = ScanForSongsRecursive(_songFolder).ToList();
+        }
+
+        private IEnumerable<SongInfo> ScanForSongsRecursive(DirectoryInfo directory)
+        {
+            List<FileInfo> chartFiles = directory.EnumerateFiles("*.chart", SearchOption.AllDirectories).ToList();
+
+            foreach (FileInfo chartFile in chartFiles)
+            {
+                Chart c = ChartFile.Load(chartFile.FullName);
+
+                if (c == null)
+                {
+                    Logger.Log($"Could not load chart {chartFile.FullName}", LogLevel.WARNING);
+                    continue;
+                }
+
+                Logger.Log($"Loaded SongInfo: {c.Artist} - {c.Title} [{c.Difficulty}]");
+                SongInfo si = new SongInfo($"{c.Artist}", $"{c.Title}", $"{c.Difficulty}", c);
+
+                yield return si;
+            }
+        }
+
+        public class SongInfo
+        {
+            public string Artist;
+            public string Name;
+            public string Difficulty;
+            public Chart chart;
+
+            public string DisplayName;
+
+            public SongInfo(string displayArtist, string displayName, string difficulty, Chart c)
+            {
+                Artist = displayArtist;
+                Name = displayName;
+                Difficulty = difficulty;
+                chart = c;
+
+                DisplayName = displayArtist + " - " + displayName + $" [{difficulty}]";
+            }
+
+            public SongInfo()
+            {
+            }
+        }
+    }
+}
+
