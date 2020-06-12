0a8c38b9edae603d35559f2cc3583ac902159fbe
Blade12629 <blade1234@web.de>
2020-06-12 03:29:36 +0200

    unity can you stop being annoying and push this, thx

---
+++ SimpleLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSServerCommon
+{
+    public class SimpleLogger : ILogger
+    {
+        public void Log(string message, int logLevel = 0, [CallerMemberName] string callerName = "")
+        {
+            Task.Run(() => Console.WriteLine($"{DateTime.UtcNow} {message}"));
+        }
+    }
+}
