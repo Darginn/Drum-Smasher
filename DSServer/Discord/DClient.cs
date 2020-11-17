c744d6a47a4ba817bba15c94ebcad97e63fd1553
Blade12629 <blade1234@web.de>
2020-11-17 21:40:25 +0100

    Added basic methods to DClient

--- DSServer/Discord/DClient.cs
+++ DSServer/Discord/DClient.cs
@@ -1,4 +1,5 @@
 ﻿using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ namespace DSServer.Discord
 {
     public static class DClient
     {
+        public const char INVISIBLE_CHAR = '‎';
         const string _DSECRET_FILE = "discord.secret";
 
         static DiscordClient _client;
@@ -25,17 +27,178 @@ namespace DSServer.Discord
 
             _client.Ready += (s, e) => Task.Run(async () => await OnReady(e));
             _client.MessageCreated += (s, e) => Task.Run(async () => await OnMessageCreated(e));
+
+            LoadCommands();
+
+            _client.ConnectAsync().ConfigureAwait(false);
+        }
+
+        static void LoadCommands()
+        {
+
+        }
+
+        /// <summary>
+        /// Sends a simple embed to a channel
+        /// </summary>
+        public static async Task<DiscordMessage> SendSimpleEmbedAsync(DiscordChannel channel, string title, string message = null)
+        {
+            DiscordEmbedBuilder builder = new DiscordEmbedBuilder()
+            {
+                Title = title,
+                Description = message
+            };
+
+            return await channel.SendMessageAsync(embed: builder.Build()).ConfigureAwait(false);
+        }
+        public static async Task<DiscordMessage> SendSimpleEmbedAsync(ulong channelId, string title, string message = null)
+        {
+            DiscordChannel channel = await GetChannelAsync(channelId).ConfigureAwait(false);
+
+            if (channel == null)
+                return null;
+
+            return await SendSimpleEmbedAsync(channel, title, message).ConfigureAwait(false);
+        }
+        public static async Task<DiscordMessage> SendSimpleEmbedAsync(DiscordGuild guild, ulong channelId, string title, string message = null)
+        {
+            DiscordChannel channel = GetChannel(guild, channelId);
+
+            if (channel == null)
+                return null;
+
+            return await SendSimpleEmbedAsync(channel, title, message).ConfigureAwait(false);
+        }
+        public static async Task<DiscordMessage> SendSimpleEmbedAsync(ulong guildId, ulong channelId, string title, string message = null)
+        {
+            DiscordGuild guild = await GetGuildAsync(guildId).ConfigureAwait(false);
+
+            if (guild == null)
+                return null;
+
+            return await SendSimpleEmbedAsync(guild, channelId, title, message);
+        }
+
+        public static async Task<DiscordGuild> GetGuildAsync(ulong id)
+        {
+            try
+            {
+                return await _client.GetGuildAsync(id).ConfigureAwait(false);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static async Task<DiscordDmChannel> GetChannelAsync(DiscordGuild guild, DiscordUser user)
+        {
+            try
+            {
+                DiscordMember member = await guild.GetMemberAsync(user.Id).ConfigureAwait(false);
+                return await member.CreateDmChannelAsync().ConfigureAwait(false);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public static async Task<DiscordDmChannel> GetChannelAsync(DiscordGuild guild, ulong userId)
+        {
+            DiscordMember member = await GetMemberAsync(guild, userId).ConfigureAwait(false);
+
+            if (member == null)
+                return null;
+
+            return await GetChannelAsync(member).ConfigureAwait(false);
+        }
+        public static async Task<DiscordDmChannel> GetChannelAsync(ulong guildId, ulong userId)
+        {
+            DiscordGuild guild = await GetGuildAsync(guildId).ConfigureAwait(false);
+
+            if (guild == null)
+                return null;
+
+            return await GetChannelAsync(guild, userId).ConfigureAwait(false);
+        }
+
+        public static async Task<DiscordDmChannel> GetChannelAsync(DiscordMember member)
+        {
+            try
+            {
+                return await member.CreateDmChannelAsync().ConfigureAwait(false);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public static async Task<DiscordChannel> GetChannelAsync(ulong id)
+        {
+            try
+            {
+                return await _client.GetChannelAsync(id).ConfigureAwait(false);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static DiscordChannel GetChannel(DiscordGuild guild, ulong id)
+        {
+            try
+            {
+                return guild.GetChannel(id);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static async Task<DiscordUser> GetUserAsync(ulong id)
+        {
+            try
+            {
+                return await _client.GetUserAsync(id).ConfigureAwait(false);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static async Task<DiscordMember> GetMemberAsync(DiscordGuild guild, ulong id)
+        {
+            try
+            {
+                return await guild.GetMemberAsync(id).ConfigureAwait(false);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public static async Task<DiscordMember> GetMemberAsync(ulong guildId, ulong id)
+        {
+            DiscordGuild guild = await GetGuildAsync(guildId).ConfigureAwait(false);
+
+            if (guild == null)
+                return null;
+
+            return await GetMemberAsync(guild, id);
         }
 
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-        private static async Task OnMessageCreated(MessageCreateEventArgs e)
+        static async Task OnMessageCreated(MessageCreateEventArgs e)
         {
             //Ignore our own messages
             if (e.Author.Id == _client.CurrentUser.Id)
                 return;
         }
 
-        private static async Task OnReady(ReadyEventArgs e)
+        static async Task OnReady(ReadyEventArgs e)
         {
         }
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
