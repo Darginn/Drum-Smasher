5a7ce2546b5f23acf33ae9ed0867a10fd672cbd3
Blade12629 <blade1234@web.de>
2020-11-17 21:16:38 +0100

    Added debug start arg

--- DSServer/Program.cs
+++ DSServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DSServer
@@ -6,6 +7,7 @@ namespace DSServer
     class Program
     {
         public const int SERVER_PORT = 40010;
+        public static bool Debug { get; private set; }
 
         static Network.Server _server;
 
@@ -16,6 +18,10 @@ namespace DSServer
         {
             try
             {
+                if (args.Length > 0 &&
+                    args.Any(arg => arg.Equals("-d", StringComparison.CurrentCultureIgnoreCase)))
+                    Debug = true;
+
                 ChatSystem.ChatRoom.InitializeChatRooms();
 
                 _server = new Network.Server(System.Net.IPAddress.Any.ToString(), SERVER_PORT);
